49206bdad3df63352e52ff8baa819f53e39ce81b
oudish <oudish.misrylall4614@gmail.com>
2019-06-08 09:10:06 +0400

    commit for second project

---
+++ ABshop/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>AB shop</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.3/animate.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/homePage.css">
+</head>
+<body>
+    <div class="clothingAutowide">
+        <div class='clothingModule'>
+            <div class='showProductInDetail' role='button' style='border-style: solid;border-width: 0px;'>
+                <img src='image/AB04.png' alt=''/>
+                <p class='productTitle' align='center'>test</p>
+                <p class='productPrice' align='center'>test</p>
+            </div>
+        </div>
+        <div class='clothingModule'>
+            <div class='showProductInDetail' role='button' style='border-style: solid;border-width: 0px;'>
+                <img src='image/AB09.png' alt=''/>
+                <p class='productTitle' align='center'>test</p>
+                <p class='productPrice' align='center'>test</p>
+            </div>
+        </div>
+        <div class='clothingModule'>
+            <div class='showProductInDetail' role='button' style='border-style: solid;border-width: 0px;'>
+                <img src='image/AB10.png' alt=''/>
+                <p class='productTitle' align='center'>test</p>
+                <p class='productPrice' align='center'>test</p>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.5/angular.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
